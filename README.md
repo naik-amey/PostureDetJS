a21e660179d9f12ba3b31827033fdd8059a483a1
Amey Naik <ameynaik@Admins-MacBook-Pro.local>
2020-12-24 10:18:03 -0800

    adding a threshold based version

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# How to run this tool
+
+1. In VS code: right click on html and select "open with Live Server" (add live server extension if you don't have it already)
+
 # PostureDetJS
 JavaScript based Posture Detection model using PosNet model. Detects if you are slouching at desk using front camera
 
@@ -12,3 +16,48 @@ Features to be added
 3. currently you are reducing the framerate, but make sure you are also running posenet model at a lower speed.
 4. or just check after 5 seconds after subject has hunched.
 5. decide if someone is in the frame/ or no one at desk. if 
+6. Use lerp for smooth interpolation and
+7. use distance from nose to eye for depth sensing. 
+https://youtu.be/EA3-k9mnLHs - coding train example. 
+
+# https://stackoverflow.com/questions/41632942/how-to-measure-time-elapsed-on-javascript/41633001
+
+
+https://ml5js.org/reference/api-PoseNet/
+https://www.w3schools.com/js/js_events.asp
+
+1. creating index.html - then type !<TAB> generates some boilerplate code for yoou in VScode.
+2. Java Script beginers tutorial - https://www.youtube.com/watch?v=W6NZfCO5SIk
+
+
+#///----------------------/// Dec 16, 2020:
+Subtasks:
+1. DrawKeypoints -
+    i. join each eye to nose. 
+    ii. join both shoulder pointers
+    iii. join nose to mid point of should points.
+
+## Useful notes:
+Id	Part
+0	nose
+1	leftEye
+2	rightEye
+3	leftEar
+4	rightEar
+5	leftShoulder
+6	rightShoulder
+7	leftElbow
+8	rightElbow
+9	leftWrist
+10	rightWrist
+11	leftHip
+12	rightHip
+13	leftKnee
+14	rightKnee
+15	leftAnkle
+16	rightAnkle
+
+## Issues:
+1. Shoulder points are not detected in the front view of the camera
+
+# https://discourse.processing.org/t/p5-loadsound-not-defined/2291
--- posenet.js
+++ posenet.js
@@ -2,6 +2,27 @@ let video;
 let poseNet;
 let poses = [];
 var started = false;
+var startTime, endTime;
+var total_hunched_time = 0; // in seconds
+var total_correct_time = 0; // in seconds
+var correct_time_before_hunch = 0; // in seconds.
+let global_nose_posX=0;
+let global_nose_posY=0;
+const HunchTimeOut = 2;
+let nose_pos;
+let l_eye_pos;
+let r_eye_pos;
+let tri_area
+let avg_eye_level
+let beep
+let corr_pos = {nose_pos: 0, l_eye: 0, r_eye: 0, tri_area: 0};
+let hunch_pos = {nose_pos: 0, l_eye: 0, r_eye: 0, tri_area: 0};
+let eye_th
+let tri_area_th
+
+function preload() {
+  beep = loadSound("beep-10.mp3"); 
+}
 
 function setup() {
   let constraints = {
@@ -10,10 +31,14 @@ function setup() {
         minWidth: 640,
         minHeight: 480
       },
-      optional: [{ maxFrameRate: 2 }]
+      //optional: [{ maxFrameRate: 2 }]
     },
     audio: false 
   };
+
+  //soundFormats('mp3', 'ogg');
+
+  // https://p5js.org/reference/#/p5/createCapture
   // createCapture(constraints, function(stream) {
   //   console.log(stream);
   // });
@@ -25,6 +50,22 @@ function setup() {
   video.size(width, height); // 640, 480
 
   // Create a new poseNet method with a single detection
+  var posenetoptions = { 
+    //architecture: 'ResNet50',
+    imageScaleFactor: 0.3,
+    outputStride: 16,
+    flipHorizontal: false,
+    minConfidence: 0.8,
+    maxPoseDetections: 1, //detect only single pose
+    scoreThreshold: 0.5,
+    nmsRadius: 20,
+    detectionType: 'single', //detect only single pose
+    multiplier: 0.75,
+  };
+  //In order to change the detectionType to single on video, you need to pass it into the constructor directly rather than in the object.
+  // https://github.com/ml5js/ml5-library/issues/355#issuecomment-500931951
+  // https://github.com/ml5js/ml5-library/pull/514#issue-294370528
+  // NOT WORKINGposeNet = ml5.poseNet(video, 'single', modelReady);
   poseNet = ml5.poseNet(video, modelReady);
   // This sets up an event that fills the global variable "poses"
   // with an array every time new poses are detected
@@ -38,12 +79,16 @@ function setup() {
 }
 
 function draw() {
+    document.getElementById('myText').innerHTML = total_hunched_time;
     if (started) {
         image(video, 0, 0, width, height);
-
         // We can call both functions to draw all keypoints and the skeletons
         drawKeypoints();
-        drawSkeleton();
+        keyPressed();
+        //check_posture();
+        //drawSkeleton();
+        //fill(0,255,0);
+        //ellipse(global_nose_posX,global_nose_posY,50);
     }
 }
 
@@ -54,37 +99,91 @@ function modelReady() {
 // A function to draw ellipses over the detected keypoints
 var defaultNosePosition = []
 function drawKeypoints()  {
+
   // Loop through all the poses detected
-  for (let i = 0; i < poses.length; i++) {
+  for (let i = 0; i < poses.length; i++) { 
     // For each pose detected, loop through all the keypoints
     let pose = poses[i].pose;
-    //console.log(pose.keypoints[0].position); 
-    let nose_pos = pose.keypoints[0].position;
-    let nose_pos_confidence = pose.keypoints[0].score;
-    if (nose_pos_confidence < 0.8) {
-      alert("nose not found")
-    }
-    //Position of eyes when a human opens experiment page. Start position.
-    while(defaultNosePosition.length < 1) {
-        console.log('in this while loop');
-        defaultNosePosition.push(nose_pos);
-        console.log(defaultNosePosition);
-      }
-    if (Math.abs(nose_pos.y - defaultNosePosition[0].y) > 25) { //nose_pos.y > 200) { //FIXME: 25???
-        alert("hunched");
-        console.log(nose_pos);
-        console.log(defaultNosePosition);
-    }
     for (let j = 0; j < pose.keypoints.length; j++) {
       // A keypoint is an object describing a body part (like rightArm or leftShoulder)
       let keypoint = pose.keypoints[j];
       // Only draw an ellipse is the pose probability is bigger than 0.8
       if (keypoint.score > 0.8) {
+      //if (keypoint.score > 0.5) {
         fill(255, 0, 0);
         noStroke();
         ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
       } 
     }
+    nose_pos = pose.keypoints[0];
+    l_eye_pos = pose.keypoints[1];
+    r_eye_pos = pose.keypoints[2];
+    stroke(0, 0, 255);
+    line(nose_pos.position.x, nose_pos.position.y, l_eye_pos.position.x, l_eye_pos.position.y);
+    line(nose_pos.position.x, nose_pos.position.y, r_eye_pos.position.x, r_eye_pos.position.y);
+    text('('+str(int(nose_pos.position.x))+','+str(int(nose_pos.position.y))+')',nose_pos.position.x, nose_pos.position.y+10)
+    text('('+str(int(l_eye_pos.position.x))+','+str(int(l_eye_pos.position.y))+')',l_eye_pos.position.x+0, l_eye_pos.position.y-10)
+    text('('+str(int(r_eye_pos.position.x))+','+str(int(r_eye_pos.position.y))+')',r_eye_pos.position.x-10, r_eye_pos.position.y-10)
+    // area of triangle
+    tri_area = abs(nose_pos.position.x*(l_eye_pos.position.y - r_eye_pos.position.y) + l_eye_pos.position.x*(r_eye_pos.position.y - nose_pos.position.y) + r_eye_pos.position.x*(nose_pos.position.y - l_eye_pos.position.y))/2;
+    avg_eye_level = (l_eye_pos.position.y + r_eye_pos.y)/2
+
+    let mid_pt_x = (nose_pos.position.x + l_eye_pos.position.x + r_eye_pos.position.x) / 3
+    let mid_pt_y = (nose_pos.position.y + l_eye_pos.position.y + r_eye_pos.position.y) / 3
+    text('('+str(int(tri_area))+')',mid_pt_x, mid_pt_y)
+    //text("HI",mid_pt_x, mid_pt_y)
+
+  }
+}
+
+function record_posture1() {
+  if (mouseIsPressed) {
+    if (mouseButton === LEFT) {
+      stroke(0, 0, 255);
+      text('('+str(int(tri_area))+')',50, 50)
+      print(mouseButton);
+      //beep.play();
+    }
+  }
+}
+
+function keyPressed() {
+  text(`${key} ${keyCode}`, 10, 40);
+  print(key, ' ', keyCode);
+  if (keyCode == 32){ // recording the correct post
+    corr_pos.nose_pos = nose_pos
+    corr_pos.l_eye = l_eye_pos.position.y
+    corr_pos.r_eye = r_eye_pos.position.y
+    corr_pos.tri_area = tri_area
+    text(corr_pos.tri_area,10,50)
+  } else if (keyCode == 40) { // down arrow
+    hunch_pos.nose_pos = nose_pos
+    hunch_pos.l_eye = l_eye_pos.position.y
+    hunch_pos.r_eye = r_eye_pos.position.y
+    hunch_pos.tri_area = tri_area
+    text(hunch_pos.l_eye,10,80)
+  } else { // any other key
+    eye_th = ((corr_pos.l_eye + hunch_pos.l_eye)/2 + (corr_pos.r_eye + hunch_pos.r_eye)/2)/2 //FIXME redundant
+    text(eye_th,10,80)
+    check_posture();
+  }
+  return false; // prevent default
+}
+
+function check_posture() {
+  // if (mouseIsPressed) {
+  //   if (mouseButton === LEFT) {
+  //     stroke(0, 0, 255);
+  //     text('('+str(int(tri_area))+')',50, 50)
+  //     print(mouseButton);
+  //     beep.play();
+  //   }
+  // }
+  //if (tri_area > 1500) {
+  let eye_level = (l_eye_pos.position.y + r_eye_pos.position.y)/2
+  if ( eye_level > eye_th) {
+    beep.play()
+    text('eye level = '+int(eye_level)+' eye_th = '+int(eye_th), 10,100)
   }
 }
 
@@ -110,6 +209,7 @@ function start() {
     started = true;
     console.log(started);
     loop();
+    starttimer();
   }
   
   // This function stops the experiment
@@ -119,4 +219,33 @@ function start() {
     removeBlur();
     started = false;
     noLoop();
+  }
+
+  function starttimer() {
+    startTime = new Date();
+  };
+  
+  function endtimer() {
+    endTime = new Date();
+    var timeDiff = endTime - startTime; //in ms
+    // strip the ms
+    timeDiff /= 1000;
+
+    console.log(startTime)
+    //console.log(endTime)
+  
+    // get seconds 
+    var seconds = Math.round(timeDiff);
+    console.log(seconds + " seconds");
+
+    correct_time_before_hunch = seconds;
+    console.log("total correct time = " + total_correct_time) 
+    console.log("correct hunch time " + correct_time_before_hunch)
+
+  }
+
+  function set_started() {
+    started = true;
+    console.log("set_started called")
+    starttimer();
   }
